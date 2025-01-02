52c4b5bca33f126b8742691232d968bab4e89dc2
Saleha Khan <161796561+Ibrahimsabir@users.noreply.github.com>
2025-01-02 15:49:24 -0800

    updated

--- src/app/productdetail/[id]/page.tsx
+++ src/app/productdetail/[id]/page.tsx
@@ -49,7 +49,7 @@ const ProductDetail = ({ params } : { params: { id: string } }) => {
             <Link href="/">
               <div className="group relative">
                 <Image
-                  src={`/{product?.image}`} // Main image
+                  src={`/${product?.img}`} // Main image
                   width={500}
                   height={500}
                   alt="Large Image"
